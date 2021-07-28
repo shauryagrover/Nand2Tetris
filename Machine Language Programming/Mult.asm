57ff8a9b2abdcc4946a796de4da21af5c993478b
@shauryagrover <30427498+shauryagrover@users.noreply.github.com>
2021-07-29 01:31:04 +0530

    Update Mult.asm

--- Machine Language Programming/Mult.asm
+++ Machine Language Programming/Mult.asm
@@ -1,20 +1,30 @@
-@R1
-D=M
-@TEMP
-M=D
 @R2
 M=0
+
+@R0
+D=M
+@END
+D;JEQ
+
+@R1
+D=M
+@END
+D;JEQ
+
+@R3
+M=D
 
 (LOOP)
   @R0
   D=M
   @R2
-  M=D+M
-  @TEMP
-  M=M-1
-  D=M
+  M=M+D
+  @R3
+  D=M-1
+  M=D
   @LOOP
   D;JGT
- (END)
-   @END
-   0;JMP
+
+(END)
+  @END
+  0;JMP
